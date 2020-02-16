7218e000a1a0bd3be73f6d155baa4c15a8ad6a03
lyyyFeng <ilinchenyu@163.com>
2020-02-16 23:08:19 +0800

    搭建spring boott

---
+++ wms-control/src/main/java/com/wms/wmscontrol/WmsControlApplication.java
@@ -0,0 +1,13 @@
+package com.wms.wmscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsControlApplication.class, args);
+    }
+
+}
